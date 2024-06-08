3cf938ab67908430289b5f811e321945c7ac5a69
BhargaviD2 <166976522+BhargaviD2@users.noreply.github.com>
2024-06-08 17:48:24 +0530

    Add files via upload

---
+++ 15th jan.c
@@ -0,0 +1,39 @@
+//Factorial of a number
+/*
+#include<stdio.h>
+int main()
+{
+    int n,i,fact=1;
+    printf("Enter a number: \n");
+    scanf("%d",&n);
+    if(n<0)
+    printf("Factorial does not exist!");
+    else if(n==0)
+    printf("The factorial of %d is 1",n)
+    else
+    {
+        for(i=1;i<=n;i++)
+        {
+            fact==fact*i;
+            
+        }
+        printf("The factorial of %d is %d",n,fact)
+    }
+    return 0
+}
+
+//Multiplication table
+#include<stdio.h>
+int main()
+{
+    int n,i,p;
+    printf("Enter a number: \n");
+    scanf("%d",&n);
+    for(i=0;i<=10;i++)
+    {
+        p=n*i;
+        printf("%d * %d = %d",n,i,p);
+    }
+    return 0;
+}
+*/
